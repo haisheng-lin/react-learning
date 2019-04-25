7002671700c22d8a1235e0fc622b0fefc302645d
haisheng-lin <linhaicheng@xiao100.com>
2019-04-25 17:23:42 +0800

    完成游戏基础功能

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -2,17 +2,47 @@ import React from 'react';
 import Square from './square.jsx';
 
 class Board extends React.Component {
+
+  constructor (props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    };
+  }
+
+  handleClick (index) {
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[index]) {
+      return;
+    }
+    squares[index] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
   /**
    * @param {number} index
    */
   renderSquare (index) {
     return (
-      <Square value={index} />
+      <Square
+        value={ this.state.squares[index] }
+        onClick={ () => this.handleClick(index) }
+      />
     );
   }
 
   render () {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
     return (
       <div>
         <div className="status">{status}</div>
@@ -34,6 +64,26 @@ class Board extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner (squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 export default Board;

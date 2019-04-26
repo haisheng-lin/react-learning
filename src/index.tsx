22567a47e45bfabc31d1f701f3004a00a7d688c3
haisheng-lin <linhaicheng@xiao100.com>
2019-04-26 16:23:01 +0800

    加入 react-router

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,34 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import './assets/styles/global.css';
 
 import Game from './components/game';
+import About from './components/about';
 
 class App extends React.Component {
 
   render () {
     return (
-      <Game />
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Game</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div>
+          <Route exact path="/" component={ Game } />
+          <Route path="/about" component={ About } />
+        </div>
+      </Router>
     );
   }
 }
